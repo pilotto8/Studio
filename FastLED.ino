4c0855c2caef61a2ce3f0fbfe40014e84ac5b894
pilotto8 <66770848+pilotto8@users.noreply.github.com>
2022-01-04 18:02:18 +0100

    Animazione ad onda

--- FastLED.ino
+++ FastLED.ino
@@ -1,39 +1,81 @@
 void setupPalette(){
     fill_solid(currentPalette, 16, CRGB::Black);
-    currentPalette[0] = CHSV(25, 190, 255);
-    currentBlending = NOBLEND;
+    CRGB custom = CHSV(27, 175, 255);
+    CRGB black  = CRGB::Black;
+    CRGB base  = CRGB::LightSeaGreen;
+    currentPalette = CRGBPalette16(
+    custom, black, black, black,
+    black, black, black, black,
+    black, black, black, black,
+    black, black, black, black
+    );
+    //currentPalette[0] = CRGB::Salmon;
+    currentBlending = LINEARBLEND;
 }
-byte steps_table[]{
-    11, 11
-};
+
+
 void RGBhandle(){
     int i;
     if (previousAnimation != animation){
         previousAnimation = animation;
-        animationSteps = steps_table[animation];
-    }
-    if (animationSteps != 0){
-        animationSteps--;
-        Serial.println(animationSteps);
+        animation_finished = 0;
+        // Setup
         switch (animation){
             case 0: {
-                if(animationSteps == 10){
-                    RGBclear();
+                for (i = 0; i < 100; i++){
+                    leds[i] = CRGB::Black;
                 }
-                else {
-                    
-                    
-                    i = 9 - animationSteps;
-                    leds[i + 10] = ColorFromPalette(currentPalette, 0, BRIGHTNESS, currentBlending);
-                    singleRow(i + 10);
-                    leds[9 - i] = ColorFromPalette(currentPalette, 0, BRIGHTNESS, currentBlending);
-                    singleRow(9 - i);
-                    break;
-                }
-                
+                fillBuffer(16);
+                LEDbuffer[9] = 15;
+                LEDbuffer[10] = 15;
+                break;
             }
         }
     }
+    if (animation_finished == 0){
+        switch (animation){
+            case 0: {
+                reportBuffer();
+                animation_finished = wave(decrease);
+                break;
+            }
+        }
+    }
+}
+
+
+void reportBuffer(){
+    int i;
+    for (i = 0; i < 20; i++){
+        leds[i] = ColorFromPalette(currentPalette, LEDbuffer[i], 255, currentBlending);
+        singleRow(i);
+    }
+}
+void fillBuffer(int value){
+    int i;
+    for(i = 0; i < 21; i++){
+        LEDbuffer[i] = value;
+    }
+}
+
+bool wave(int mode){
+    int i, c;
+    bool completed = 1;
+    for (i = 0; i < 20; i++){
+        if (LEDbuffer[i] != 0 && LEDbuffer[i] != 16){
+            completed = 0;
+            LEDbuffer[i] += mode;
+            for (c = -1; c <= 1; c += 2){
+                if (LEDbuffer[i + c] == 0 && mode == increase || LEDbuffer[i + c] == 16 && mode == decrease){
+                    LEDbuffer[i + c] += mode;
+                    if (c == 1){
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+    return completed;
 }
 
 void singleRow(int i){
@@ -43,6 +85,7 @@ void singleRow(int i){
     leds[79 - i] = leds[i];
 }
 void RGBclear(){
+    int i;
     for (i = 0; i < 100; i++){
         leds[i] = CRGB::Black;
     }
--- Settings.h
+++ Settings.h
@@ -6,9 +6,14 @@
 #define LED_TYPE    WS2811
 #define COLOR_ORDER BRG
 CRGB leds[NUM_LEDS];
-#define UPDATES_PER_SECOND 20
+#define UPDATES_PER_SECOND 40
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
 byte animation = 0;
 byte previousAnimation = -1;
-byte animationSteps = 0;
+bool animation_finished = 0;
+int LEDbuffer[21];
+enum modes {
+    decrease = -1,
+    increase = 1        
+};
