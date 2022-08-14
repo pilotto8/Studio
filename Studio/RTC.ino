f401b6766540385d4b684bae770af427ff49dfce
Mattia Pilotto <66770848+pilotto8@users.noreply.github.com>
2022-08-14 18:26:24 +0200

    Starting alarm management

--- Studio/RTC.ino
+++ Studio/RTC.ino
@@ -1,3 +1,36 @@
 void getTime(){
     now = rtc.now();
 }
+
+void setAlarm(byte hour, byte minute, byte plugs, byte state){
+    
+}
+
+struct {
+    byte plugs;
+    bool state;
+    DateTime alarm;
+    byte days;
+}rtc_trigg[8];
+
+byte active_trigg;
+
+void setBits(int* byt, byte a, bool bit){
+    byte temp = 1;
+    temp =<< a; 
+    if (bit){
+        *byt |= temp;
+    }
+    else {
+        *byt -= (*byt & temp);
+    }
+}
+
+
+bool readBits(int* byt, byte a){
+    byte temp = *byt << a;
+    if (temp >> 7){
+        return 1;
+    }
+    return 0;
+}
